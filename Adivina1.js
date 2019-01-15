3ecc48c574173a823df6e3540e1c00947b502646
Mugetsu145 <45347111+Mugetsu145@users.noreply.github.com>
2019-01-15 11:10:04 -0600

    Add files via upload

---
+++ Adivina1.js
@@ -0,0 +1,256 @@
+
+function tell(){
+var max = event.keyCode;
+if(max == 13){
+please()
+}
+}
+
+function alert(){
+var simple = document.getElementById('sai')
+simple.remove()
+
+}
+
+//To do the reset button I have to reset the global variable
+function italia(){
+	tetris = 'hello'
+var crear = document.getElementById('jose')
+crear.remove()
+}
+function rusia(){
+	document.location.reload()
+}
+
+
+function tempo(){
+
+var song = document.getElementById("tesoro")
+song.play()
+yes = 30
+
+
+constante = 29
+function lampara(){
+if(constante > -1){
+ document.getElementById('gato').innerHTML = constante + ' segundos';
+constante = constante - 1 
+}
+}
+setInterval(lampara,1000)
+}
+
+function segundo(){
+
+country = 0
+document.getElementById('lol').innerHTML = country +"/12";
+
+canada = "canada"
+estadosunidos = "estados unidos"
+Mexico= "mexico"
+Brazil = "brasil"
+Guatemala= "guatemala"
+Argentina = "argentina"
+Colombia = "colombia"
+Bolivia = "bolivia"
+Peru = "peru"
+Venezuela = "venezuela"
+Cuba = "cuba"
+Chile = "chile"
+
+
+}
+
+
+
+function paper(){
+canada = 891012721
+estadosunidos = 891012721
+Mexico= 891012721
+Brazil = 891012721
+Guatemala= 891012721
+Argentina = 891012721
+Colombia = 891012721
+Bolivia = 891012721
+Peru = 891012721
+Venezuela = 891012721
+Cuba = 891012721
+Chile = 891012721
+
+	alert("Se acabo el tiempo" + "\n" + "Sacaste " + country +"/12")
+}
+
+setTimeout(paper,1000*31)
+
+
+function please(){
+var puedes = document.getElementById('correcto')
+var x = document.getElementById("canvas")
+canvas = x.getContext('2d')
+var si = document.getElementById("luna").value
+var si = si.toLowerCase()
+canvas.fillStyle = "green";
+
+if(si == canada){
+canvas.fillRect(600,250,70,70)
+ country 
+  country = country + 1
+ document.getElementById('lol').innerHTML = country+"/12";
+canada
+canada = "asdasd"
+puedes.play()
+document.getElementById('luna').value = '';
+}
+
+if(si == estadosunidos){
+canvas.fillRect(525,425,80,80)
+country 
+country = country + 1
+document.getElementById('lol').innerHTML = country+"/12";
+estadosunidos
+estadosunidos = "aqwekqwe"
+puedes.play()
+	 document.getElementById('luna').value = '';
+
+}
+if(si == Mexico){
+canvas.fillRect(525,600,40,40)
+country 
+country = country + 1
+ document.getElementById('lol').innerHTML = country+"/12";
+Mexico
+Mexico ="qwieq0we"
+puedes.play()
+ document.getElementById('luna').value = '';
+
+}
+if(si == Guatemala){
+canvas.fillRect(600,660,15,15)
+country
+country = country + 1
+ document.getElementById('lol').innerHTML = country+"/12";
+Guatemala
+Guatemala = "asqo12012"
+puedes.play()
+	 document.getElementById('luna').value = '';
+
+}
+if(si == Brazil){
+canvas.fillRect(870,880,80,80)
+country
+country = country + 1
+ document.getElementById('lol').innerHTML = country+"/12";
+ Brazil
+ Brazil = "jqo1912"
+ puedes.play()
+ 	 document.getElementById('luna').value = '';
+
+}
+
+if(si== Argentina){
+canvas.fillRect(800,1050,60,60)//Argentina
+country
+country = country + 1
+ document.getElementById('lol').innerHTML = country+"/12";
+ Argentina
+ Argentina ="as8919"
+ puedes.play()
+ 	 document.getElementById('luna').value = '';
+
+}
+
+if(si == Colombia){
+canvas.fillRect(720,750,35,35)//Colombia
+country
+country = country + 1
+ document.getElementById('lol').innerHTML = country+"/12";
+ Colombia
+ Colombia ="as8919"	
+ puedes.play()
+ 	 document.getElementById('luna').value = '';
+
+}
+if(si == Bolivia){
+canvas.fillRect(790,930,40,40)//Colombia
+country
+country = country + 1
+ document.getElementById('lol').innerHTML = country+"/12";
+ Bolivia
+ Bolivia ="as8919"	
+ puedes.play()
+ 	 document.getElementById('luna').value = '';
+
+}
+if(si == Peru){
+canvas.fillRect(700,850,25,25)//Peru
+country
+country = country + 1
+ document.getElementById('lol').innerHTML = country+"/12";
+ Peru
+ Peru ="as8919"	
+ puedes.play()
+ 	 document.getElementById('luna').value = '';
+
+}
+if(si == Venezuela){
+canvas.fillRect(790,730,25,25)
+country
+country = country + 1
+ document.getElementById('lol').innerHTML = country+"/12";
+ Venezuela
+ Venezuela ="as8919"	
+ puedes.play()
+ 	 document.getElementById('luna').value = '';
+
+}
+if(si == Cuba){
+canvas.fillRect(700,600,10,10)
+country
+country = country + 1
+ document.getElementById('lol').innerHTML = country+"/12";
+ Cuba
+ Cuba ="as8919"	
+ puedes.play()
+ 	 document.getElementById('luna').value = '';
+
+}
+if(si == Chile){
+canvas.fillRect(772,1020,10,25)
+country
+country = country + 1
+ document.getElementById('lol').innerHTML = country+"/12";
+ Chile
+ chile ="as8919"	
+ puedes.play()
+ 	 document.getElementById('luna').value = '';
+
+}
+}
+
+
+function dos(){
+var y = document.getElementById("canada")
+skate = y.getContext('2d')
+skate.fillStyle = "gray";
+skate.fillRect(525,425,80,80)//Canada
+skate.fillRect(600,250,70,70)//US
+skate.fillRect(525,600,40,40)//Mexico
+skate.fillRect(600,660,15,15)//Guatemala
+skate.fillRect(870,880,80,80)//Brazil
+skate.fillRect(800,1050,60,60)//Argentina
+skate.fillRect(720,750,35,35)//Colombia
+skate.fillRect(790,930,40,40)//Bolivia
+skate.fillRect(700,850,25,25)//Peru
+skate.fillRect(790,730,25,25)//Venezuela
+skate.fillRect(700,600,10,10)//Cuba
+skate.fillRect(772,1020,10,25)//Chile
+}
+window.addEventListener("load",dos, false)
+
+
+
+
+
+
+//Sub elements are textbox in html
+
---
+++ Adivina2.js
@@ -0,0 +1,683 @@
+
+
+
+function tell(){
+var max = event.keyCode;
+if(max == 13){
+please()
+}
+}
+
+
+function italia(){
+	tetris = 'hello'
+var crear = document.getElementById('jose')
+crear.remove()
+}
+
+function rusia(){
+	document.location.reload()
+}
+
+function tempo(){
+var song = document.getElementById("tesoro")
+song.play()
+
+
+Time = 0
+constante = 29
+function lampara(){
+if(constante > 20){
+ document.getElementById('gato').innerHTML = Time + ' segundos';
+Time = Time + 1
+
+}
+}
+setInterval(lampara,1000)
+
+
+country = 0
+document.getElementById('lol').innerHTML = country +"/12";
+
+
+
+
+Canada = "canada"
+estadosunidos = "estados unidos"
+Mexico= "mexico"
+Brazil = "brasil"
+Guatemala= "guatemala"
+Argentina = "argentina"
+Colombia = "colombia"
+Bolivia = "bolivia"
+Peru = "peru"
+Venezuela = "venezuela"
+Cuba = "cuba"
+Chile = "chile"
+
+
+
+
+
+Mar =  Math.floor(Math.random()*8)
+
+Pet = 1
+Nectar = 1
+Estado = 1
+
+
+Tar = 0
+Jones = 1
+Jar = 2
+Peri = 3
+Menino = 4
+Dab = 5
+Sing = 6
+Bat = 7
+
+if( Mar == Tar){
+	map.fillRect(790,930,40,40)//Bolivia
+}
+if(Mar == Jones){
+	map.fillRect(600,250,70,70)//Canada
+}
+if( Mar == Jar){
+	map.fillRect(515,600,40,40)//Mexico
+}
+if( Mar == Peri){//Brasil
+map.fillRect(870,880,80,80)
+}
+if (Mar == Menino){//Guatemala
+map.fillRect(600,660,15,15)
+}
+if (Mar == Dab){
+map.fillRect(800,1050,60,60)//Argentina
+}
+if(Mar == Sing){
+map.fillRect(720,750,35,35)//Colombia
+}
+if(Mar == Bat){
+map.fillRect(700,850,25,25)//Peru
+}
+
+
+
+
+
+}
+
+
+function please(){
+var puedes = document.getElementById('correcto')
+var si = document.getElementById("luna").value
+var x = document.getElementById("canvas")
+canvas = x.getContext('2d')
+
+
+
+var h = document.getElementById("map")
+map = h.getContext('2d')
+map.fillStyle = "blue";
+canvas.fillStyle = "green";
+
+
+
+
+if (Mar == Tar){
+map.fillRect(790,930,40,40)//Bolivia
+if(si == Bolivia){
+	puedes.play()
+canvas.fillRect(790,930,40,40)//Bolivia
+country
+country = country + 1
+ document.getElementById('lol').innerHTML = country+"/12";
+ Bolivia ="as8919"	
+ Tar = 92
+ document.getElementById('luna').value = '';
+for(let Kar = 0; Kar < 50; Kar++){	
+Mar = Math.floor(Math.random()*8)
+
+if( Mar == Jones && Jones == 1){
+	map.fillRect(600,250,70,70)//Canada
+	break
+}
+ if(Mar == Jar && Jar == 2){
+map.fillRect(515,600,40,40)//Mexico
+break
+}
+if(Mar == Peri && Peri == 3){
+map.fillRect(870,880,80,80)
+break
+}
+if(Mar == Menino && Menino == 4){
+	map.fillRect(600,660,15,15)
+break
+}
+if(Mar == Dab && Dab == 5){
+map.fillRect(800,1050,60,60)
+break	
+}
+if(Mar == Sing && Sing == 6){
+map.fillRect(720,750,35,35)//Colombia
+break
+}
+if(Mar == Bat && Bat == 7){
+map.fillRect(700,850,25,25)//Peru
+}
+
+}
+}
+}
+
+
+
+if (Mar == Jones){
+map.fillRect(600,250,70,70)//Canada
+if(si == Canada){
+puedes.play()
+canvas.fillRect(600,250,70,70)//Canada
+country
+country = country + 1
+ document.getElementById('lol').innerHTML = country+"/12";
+ Canada ="as8919"
+Jones = 67
+document.getElementById('luna').value = '';
+for( Pet; Pet < 50; Pet++){	
+Mar = Math.floor(Math.random()*8)
+if( Mar == Tar && Tar == 0){
+	map.fillRect(790,930,40,40)//Bolivia
+	break
+}
+ if(Mar == Jar && Jar == 2){
+map.fillRect(515,600,40,40)//Mexico
+break
+}
+
+if(Mar == Peri && Peri == 3){
+map.fillRect(870,880,80,80)
+break
+}
+if(Mar == Menino && Menino == 4){
+	map.fillRect(600,660,15,15)
+break
+}
+if(Mar == Dab && Dab == 5){
+map.fillRect(800,1050,60,60)
+break	
+}
+if(Mar == Sing && Sing == 6){
+map.fillRect(720,750,35,35)//Colombia
+break
+}
+if(Mar == Bat && Bat == 7){
+map.fillRect(700,850,25,25)//Peru
+}
+
+}
+}
+}
+
+
+
+if (Mar == Jar){
+map.fillRect(515,600,40,40)//Mexcio
+if(si == Mexico){
+puedes.play()
+canvas.fillRect(515,600,40,40)//Mexcio
+country
+country = country + 1
+ document.getElementById('lol').innerHTML = country+"/12";
+ Mexico ="as8919"	
+ Jar = 84
+ document.getElementById('luna').value = '';
+
+for(Nectar; Nectar < 50; Nectar++){	
+Mar = Math.floor(Math.random()*8)	
+if( Mar == Tar && Tar == 0){
+	map.fillRect(790,930,40,40)//Bolivia
+	break
+}
+ if(Mar == Jones && Jones == 1){
+	map.fillRect(600,250,70,70)//Canada
+    break
+}
+if(Mar == Peri && Peri == 3){
+map.fillRect(870,880,80,80)
+break
+}
+if(Mar == Menino && Menino == 4){
+	map.fillRect(600,660,15,15)
+break
+}
+if(Mar == Dab && Dab == 5){
+map.fillRect(800,1050,60,60)
+break	
+}
+if(Mar == Sing && Sing == 6){
+map.fillRect(720,750,35,35)//Colombia
+break
+}
+if(Mar == Bat && Bat == 7){
+map.fillRect(700,850,25,25)//Peru
+}
+
+}
+}
+}
+
+
+
+
+if (Mar == Peri){
+map.fillRect(870,880,80,80)//Brazil
+if(si == Brazil){
+puedes.play()
+canvas.fillRect(870,880,80,80)
+country
+country = country + 1
+ document.getElementById('lol').innerHTML = country+"/12";
+ Brazil = "jqo1912"
+ Peri = 101
+ document.getElementById('luna').value = '';
+
+for(Estado; Estado < 50; Estado++){	
+Mar = Math.floor(Math.random()*8)	
+if( Mar == Tar && Tar == 0){
+	map.fillRect(790,930,40,40)//Bolivia
+	break
+}
+ if(Mar == Jones && Jones == 1){
+	map.fillRect(600,250,70,70)//Canada
+    break
+}
+
+ if(Mar == Jar && Jar == 2){
+map.fillRect(515,600,40,40)//Mexico
+break
+}
+if(Mar == Menino && Menino == 4){
+	map.fillRect(600,660,15,15)
+break
+}
+if(Mar == Dab && Dab == 5){
+map.fillRect(800,1050,60,60)
+break	
+}
+if(Mar == Sing && Sing == 6){
+map.fillRect(720,750,35,35)//Colombia
+break
+}
+if(Mar == Bat && Bat == 7){
+map.fillRect(700,850,25,25)//Peru
+}
+
+}
+}
+}
+
+
+if (Mar == Menino){//guatemala
+map.fillRect(600,660,15,15)
+if(si == Guatemala){
+puedes.play()
+canvas.fillRect(600,660,15,15)
+country
+country = country + 1
+ document.getElementById('lol').innerHTML = country+"/12";
+ Guatemala = "jqo1912"
+ Menino = 170
+ document.getElementById('luna').value = '';
+
+for(Estado; Estado < 50; Estado++){	
+Mar = Math.floor(Math.random()*8)	
+if( Mar == Tar && Tar == 0){
+	map.fillRect(790,930,40,40)//Bolivia
+	break
+}
+ if(Mar == Jones && Jones == 1){
+	map.fillRect(600,250,70,70)//Canada
+    break
+}
+
+ if(Mar == Jar && Jar == 2){
+map.fillRect(515,600,40,40)//Mexico
+break
+}
+if(Mar == Peri && Peri == 3){
+map.fillRect(870,880,80,80)
+break
+}
+if(Mar == Dab && Dab == 5){
+map.fillRect(800,1050,60,60)
+break	
+}
+if(Mar == Sing && Sing == 6){
+map.fillRect(720,750,35,35)//Colombia
+break
+}
+if(Mar == Bat && Bat == 7){
+map.fillRect(700,850,25,25)//Peru
+}
+
+}
+}
+}
+
+
+if (Mar == Dab){//Argentina
+map.fillRect(800,1050,60,60)
+if(si == Argentina){
+puedes.play()
+canvas.fillRect(800,1050,60,60)
+country
+country = country + 1
+ document.getElementById('lol').innerHTML = country+"/12";
+ Argentina = "jqo1912"
+ Dab = 220
+ document.getElementById('luna').value = '';
+for(let Kar = 0; Kar < 50; Kar++){	
+Mar = Math.floor(Math.random()*8)	
+if( Mar == Tar && Tar == 0){
+	map.fillRect(790,930,40,40)//Bolivia
+	break
+}
+ if(Mar == Jones && Jones == 1){
+	map.fillRect(600,250,70,70)//Canada
+    break
+}
+
+ if(Mar == Jar && Jar == 2){
+map.fillRect(515,600,40,40)//Mexico
+break
+}
+if(Mar == Peri && Peri == 3){
+map.fillRect(870,880,80,80)//Brazil
+break
+}
+if(Mar == Menino && Menino == 4){
+	map.fillRect(600,660,15,15)//Guatemala
+break
+}
+if(Mar == Sing && Sing == 6){
+map.fillRect(720,750,35,35)//Colombia
+break
+}
+if(Mar == Bat && Bat == 7){
+map.fillRect(700,850,25,25)//Peru
+}
+
+}
+}
+}
+
+if (Mar == Sing){//Colombia
+map.fillRect(720,750,35,35)//Colombia
+if(si == Colombia){
+puedes.play()
+canvas.fillRect(720,750,35,35)//Colombia
+country
+country = country + 1
+ document.getElementById('lol').innerHTML = country+"/12";
+ Colombia = "jqo1912"
+ Sing = 280
+ document.getElementById('luna').value = '';
+for(let Kar = 0; Kar < 50; Kar++){	
+Mar = Math.floor(Math.random()*8)	
+if( Mar == Tar && Tar == 0){
+	map.fillRect(790,930,40,40)//Bolivia
+	break
+}
+ if(Mar == Jones && Jones == 1){
+	map.fillRect(600,250,70,70)//Canada
+    break
+}
+
+ if(Mar == Jar && Jar == 2){
+map.fillRect(515,600,40,40)//Mexico
+break
+}
+if(Mar == Peri && Peri == 3){
+map.fillRect(870,880,80,80)//Brazil
+break
+}
+if(Mar == Menino && Menino == 4){
+	map.fillRect(600,660,15,15)//Guatemala
+break
+}
+if(Mar == Dab && Dab == 5){
+map.fillRect(800,1050,60,60)
+break	
+}
+if(Mar == Bat && Bat == 7){
+map.fillRect(700,850,25,25)//Peru
+}
+
+}
+}
+}
+
+if (Mar == Bat){//Peru
+map.fillRect(700,850,25,25)//Peru
+if(si == Peru){
+puedes.play()
+canvas.fillRect(700,850,25,25)//Peru
+country
+country = country + 1
+ document.getElementById('lol').innerHTML = country+"/12";
+ Peru = "jqo1912"
+ Bat = 310
+ document.getElementById('luna').value = '';
+for(let Kar = 0; Kar < 50; Kar++){	
+Mar = Math.floor(Math.random()*8)	
+if( Mar == Tar && Tar == 0){
+	map.fillRect(790,930,40,40)//Bolivia
+	break
+}
+ if(Mar == Jones && Jones == 1){
+	map.fillRect(600,250,70,70)//Canada
+    break
+}
+
+ if(Mar == Jar && Jar == 2){
+map.fillRect(515,600,40,40)//Mexico
+break
+}
+if(Mar == Peri && Peri == 3){
+map.fillRect(870,880,80,80)//Brazil
+break
+}
+if(Mar == Menino && Menino == 4){
+	map.fillRect(600,660,15,15)//Guatemala
+break
+}
+if(Mar == Dab && Dab == 5){
+map.fillRect(800,1050,60,60)//Argentina
+break	
+}
+if(Mar == Sing && Sing == 6){
+map.fillRect(720,750,35,35)//Colombia
+break
+}
+}
+}
+}
+
+
+
+if(Sing == 280){
+if(Dab == 220){
+if(Menino == 170){
+if( Tar == 92){
+	if(Peri == 101){
+	if(Jar == 84){
+if(Jones == 67){
+constante = 15
+Time = Time - 1
+alert("se acabo el juego, nombraste los paises en" + "\n" + Time + " segundos" )
+}}}}}}}
+//Eso pasa cuando cay en el numero que desaparecio
+
+
+//no se esta cambiando valor
+
+/*
+var x = document.getElementById("canvas")
+canvas = x.getContext('2d')
+var si = document.getElementById("luna").value
+canvas.fillStyle = "green";
+
+
+if(si == canada){
+canvas.fillRect(600,250,70,70)
+ country 
+  country = country + 1
+ document.getElementById('lol').innerHTML = country+"/12";
+canada
+canada = "asdasd"
+}
+
+if(si == estadosunidos){
+canvas.fillRect(525,425,80,80)
+country 
+country = country + 1
+document.getElementById('lol').innerHTML = country+"/12";
+estadosunidos
+estadosunidos = "aqwekqwe"
+}
+if(si == Mexico){
+canvas.fillRect(515,600,40,40)
+country 
+country = country + 1
+ document.getElementById('lol').innerHTML = country+"/12";
+Mexico
+Mexico ="qwieq0we"
+}
+if(si == Guatemala){
+canvas.fillRect(600,660,15,15)
+country
+country = country + 1
+ document.getElementById('lol').innerHTML = country+"/12";
+Guatemala
+Guatemala = "asqo12012"
+}
+if(si == Brazil){
+canvas.fillRect(870,880,80,80)
+country
+country = country + 1
+ document.getElementById('lol').innerHTML = country+"/12";
+ Brazil
+ Brazil = "jqo1912"
+}
+
+if(si== Argentina){
+canvas.fillRect(800,1050,60,60)//Argentina
+country
+country = country + 1
+ document.getElementById('lol').innerHTML = country+"/12";
+ Argentina
+ Argentina ="as8919"
+}
+
+if(si == Colombia){
+canvas.fillRect(720,750,35,35)//Colombia
+country
+country = country + 1
+ document.getElementById('lol').innerHTML = country+"/12";
+ Colombia
+ Colombia ="as8919"	
+}
+if(si == Bolivia){
+canvas.fillRect(790,930,40,40)//Colombia
+country
+country = country + 1
+ document.getElementById('lol').innerHTML = country+"/12";
+ Bolivia
+ Bolivia ="as8919"	
+}
+if(si == Peru){
+canvas.fillRect(700,850,25,25)//Peru
+country
+country = country + 1
+ document.getElementById('lol').innerHTML = country+"/12";
+ Peru
+ Peru ="as8919"	
+}
+if(si == Venezuela){
+canvas.fillRect(790,730,25,25)
+country
+country = country + 1
+ document.getElementById('lol').innerHTML = country+"/12";
+ Venezuela
+ Venezuela ="as8919"	
+}
+if(si == Cuba){
+canvas.fillRect(700,600,10,10)
+country
+country = country + 1
+ document.getElementById('lol').innerHTML = country+"/12";
+ Cuba
+ Cuba ="as8919"	
+}
+if(si == Chile){
+canvas.fillRect(772,1020,10,25)
+country
+country = country + 1
+ document.getElementById('lol').innerHTML = country+"/12";
+ Chile
+ chile ="as8919"	
+
+}
+*/
+
+}
+window.addEventListener("load",please, false)
+
+
+
+
+function dos(){
+var y = document.getElementById("canada")
+skate = y.getContext('2d')
+skate.fillStyle = "gray";
+
+skate.fillRect(525,425,80,80)//Canada
+skate.fillRect(600,250,70,70)//US
+skate.fillRect(515,600,40,40)//Mexico
+skate.fillRect(600,660,15,15)//Guatemala
+skate.fillRect(870,880,80,80)//Brazil
+skate.fillRect(800,1050,60,60)//Argentina
+skate.fillRect(720,750,35,35)//Colombia
+skate.fillRect(790,930,40,40)//Bolivia
+skate.fillRect(700,850,25,25)//Peru
+skate.fillRect(790,730,25,25)//Venezuela
+skate.fillRect(700,600,10,10)//Cuba
+skate.fillRect(772,1020,10,25)//Chile
+}
+window.addEventListener("load",dos, false)
+
+
+
+function cincoMinutos(){
+	alert("Han transcurrido 5 minutos")
+}
+
+
+
+//If you put var, it is local, if you make a variable without var it is global
+
+/************************
+dificultades que encontre haciendo esto:
+1. No podia tener dos colores a la vez, por que la funcion canvas.Fill llenaba todo, lo que
+hize fue hacer dos canvas y poner uno sobre otro y asi un color va asignado a cada canvas
+2. Var, let, const, las variables son muy especificas, si no esta el rango global o local exactamente
+como se debe, no va a funciona. Gaste cuarenta minutos intentando hacer una implemenctacion que lidio con esto
+mi solucion es muy tonta.
+
+HTML:
+
+The ::placeholder CSS pseudo-element represents the placeholder text of a form element.
+::placeholder { color: blue; font-size: 1.5em; }
+input::placeholder { color: red; font-size: 1.2em; font-style: italic; }
+input::placeholder { color: green; }
+
+***********************/
